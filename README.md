109e2746ddce9534fd0ef78a2754bd60f1d4a286
Mike Dubman <miked@mellanox.com>
2020-08-22 11:55:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,97 @@
+# Matrix workflow project for CI/CD
+
+This is Jenkins CI/CD demo project. You can create custom workflows to automate your project software life cycle process. 
+
+You need to configure workflows using YAML syntax, and save them as workflow files in your repository. Once you've successfully created a YAML workflow file and triggered the workflow - Jenkins parse flow and execute it.
+
+
+## Quick start
+
+1. Create ```.ci/Jenkinsfile``` with content:
+
+``` groovy
+#!/usr/bin/groovy
+
+// load pipeline functions
+// Requires pipeline-github-lib plugin to load library from github
+@Library('github.com/Mellanox/ci-demo')
+def matrix = new com.mellanox.cicd.Matrix()
+
+matrix.main()
+```
+2. Create ```.ci/matrix_job.yaml``` basic workflow file with content:
+
+``` yaml
+kubernetes:
+  cloud: swx-k8s
+  nodeSelector: 'beta.kubernetes.io/os=linux'
+
+runs_on_dockers:
+  - {name: 'ubuntu16-4', tag: 'latest'}
+
+matrix:
+  axes:
+    flags:
+      - '--enable-debug'
+      - '--prefix=/tmp/install'
+    arch:
+      - x86_64
+steps:
+
+  - name: Configure
+    run: |
+      ./autogen.sh
+      ./configure $flags
+
+  - name: Build
+    run: make -j 2 all
+
+  - name: Install
+    run: make -j 2 install
+     
+    finalize:
+      run: echo All done
+```
+
+3. Register Jenkins project manually via Jenkins UI or with JJB file as below
+
+``` bash
+$ sudo jenkins-jobs update proj_jjb.yaml
+```
+
+4. Trigger run via Jenkins UI
+
+## Matrix job example
+
+![Alt text](.ci/pict/snapshot2.png?raw=true "Matrix Job")
+
+
+## Important files
+
+The CI scripts are located in ```.ci``` folder
+Jenkins behavior can be controlled by job_matrix.yaml file which has similar syntax/approach as Github actions.
+
+* jenkinsfile parses .ci/job_matrix*.yaml files
+* the job is bootstrapped and executing according to steps, as defined in yaml file
+* The method works with vanilla jenkins [Jenkinsfile](.ci/Jenkinsfile)
+* The method also works with pipeline as shared library [Jenkinsfile.shlib](.ci/Jenkinsfile.shlib) and uses this [shlib](src/com/mellanox/cicd/Matrix.groovy)
+
+* [Basic job](.ci/job_matrix_basic.yaml) definition can be found at ```.ci/job_matrix_basic.yaml```
+* [Advanced job](.ci/job_matrix.yaml) definition is at ```.ci/job_matrix.yaml```
+* The actual [Jenkinsfile](.ci/Jenkinsfile) that gets executed is here at ```.ci/Jenkinsfile```
+
+
+## Job matrix file
+
+* Job file can contain array of steps, which are executed sequentially in the docker
+* Job file can define own matrix and Jenkinsfile will generate axis combinations to execute in the docker
+* Job file can define list of dockerfiles, in ```.ci/Dockerfiles.*``` that will be added to matrix combinations.
+* Job file can define if need to build docker from dockerfiles and push resulting image to the registry or just fetch image from registry.
+* Job can define different [environment](.ci/job_matrix.yaml#L13) variables that will be added to step run environment
+* Job can define optional [include/exclude](.ci/job_matrix.yaml#L36) filters to select desired matrix dimensions
+
+## Jenkins job builder
+
+* Demo contains [Jenkins Job Builder](.ci/jjb_proj.yaml) config file to load Jenkins project definition into Jenkins server
+
+
---
+++ src/com/mellanox/cicd/Matrix.groovy
@@ -0,0 +1,347 @@
+#!/usr/bin/groovy
+package com.mellanox.cicd;
+
+@NonCPS
+List getMatrixAxes(matrix_axes) {
+    List axes = []
+    matrix_axes.each { axis, values ->
+        List axisList = []
+        values.each { value ->
+            axisList << [(axis): value]
+        }
+        axes << axisList
+    }
+    // calculate cartesian product
+    axes.combinations()*.sum()
+}
+
+def run_shell(cmd, title) {
+    sh(script: cmd, label: title)
+}
+
+def forceCleanupWS() {
+    env.WORKSPACE = pwd()
+    def cmd = """
+    rm -rf ${env.WORKSPACE}/*
+    find ${env.WORKSPACE}/ -maxdepth 1 -name '.*' | xargs rm -rf 
+    """
+    run_shell(cmd, "Clean workspace")
+}
+
+def gen_image_map(config) {
+    def image_map = [:]
+
+    if (config.get("matrix") && config.matrix.axes.arch) {
+        for (arch in config.matrix.axes.arch) {
+            image_map[arch] = []
+        }
+    } else {
+        for (dfile in config.runs_on_dockers) {
+            if (dfile.arch) {
+                image_map["${dfile.arch}"] = []
+            }
+        }
+    }
+
+
+    image_map.each { arch, images ->
+        config.runs_on_dockers.each { dfile ->
+            if (!dfile.file) {
+                dfile.file = ""
+            }
+            def item = [\
+                arch: "${arch}", \
+                tag:  "${dfile.tag}", \
+                filename: "${dfile.file}", \
+                url: "${config.registry_host}${config.registry_path}/${arch}/${dfile.name}:${dfile.tag}", \
+                name: "${dfile.name}" \
+            ]
+            echo "[INFO] Adding docker to image_map for " + arch + " name: " + item.name
+            images.add(item)
+        }
+    }
+    return image_map
+}
+
+def matchMapEntry(filters, entry) {
+    def match
+    for (filter in filters) {
+        match = 1
+        filter.each { k,v ->
+            if (v != entry[k]) {
+                match = 0
+                return
+            }
+        }
+        if (match) {
+            break
+        }
+    }
+    return match
+}
+
+def onUnstash() {
+
+    env.PATH = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+    env.WORKSPACE = pwd()
+
+    def cmd = """#!/bin/bash -l
+    hash -r
+    tar xf scm-repo.tar
+    git reset --hard
+    rm -f scm-repo.tar
+    """
+    run_shell(cmd, "Extracting project files into workspace")
+}
+
+
+def runSteps(config) {
+    forceCleanupWS()
+    // fetch .git from server and unpack
+    unstash "${env.JOB_NAME}"
+    onUnstash()
+
+    def shell = getConfigVal(config, ['shell'], '#!/bin/bash -leE')
+    config.steps.each { one->
+        echo "Step: ${one.name}"
+        def cmd = """${shell}
+        ${one.run}
+        """
+        run_shell(cmd, one.name)
+    }
+}
+
+def getDockerOpt(config) {
+    def opts = getConfigVal(config, ['docker_opt'], "")
+    if (config.get("volumes")) {
+        for (vol in config.volumes) {
+            hostPath = vol.get("hostPath")? vol.hostPath : vol.mountPath
+            opts += " -v ${vol.mountPath}:${hostPath}"
+        }
+    }
+    return opts
+}
+
+def getConfigVal(config, list, defaultVal) {
+    def val = config
+    for (item in list) {
+        val = val.get(item)
+        if (!val) {
+            return defaultVal
+        }
+    }
+
+    def ret =  (val instanceof ArrayList)? val[0] : val
+    return ret
+}
+
+def runK8(image, branchName, config, axis) {
+
+    def cloudName = getConfigVal(config, ['kubernetes','cloud'], "")
+
+    println("[INFO] Running kubernetes ${cloudName}")
+
+    def listV = []
+    for (vol in config.volumes) {
+        hostPath = vol.get("hostPath")
+        mountPath = vol.get("mountPath")
+        hpv = hostPathVolume(hostPath: hostPath, mountPath: mountPath)
+        listV.add(hpv)
+    }
+
+    def cname = image.get("name").replaceAll("[\\.:/_]","")
+    def nodeSelector = getConfigVal(config, ['kubernetes', 'nodeSelector'], "")
+
+    podTemplate(cloud: cloudName, runAsUser: "0", runAsGroup: "0",
+                nodeSelector: nodeSelector,
+                containers: [
+                    containerTemplate(name: cname, image: image.url, ttyEnabled: true, alwaysPullImage: true, command: 'cat')
+                ],
+                volumes: listV
+                )
+    {
+        node(POD_LABEL) {
+            stage (branchName) {
+                container(cname) {
+                    runSteps(config)
+                }
+            }
+        }
+    }
+}
+
+def resolveTemplate(varsMap, str) {
+    GroovyShell shell = new GroovyShell(new Binding(varsMap))
+    return shell.evaluate('"' + str +'"')
+}
+
+def runDocker(image, branchName, config, axis) {
+    def label = getConfigVal(config, ['docker_node'], '${arch} && docker')
+    def nodeName = resolveTemplate(axis, label)
+
+    println("[INFO] Running docker on ${nodeName}")
+
+    node(nodeName) {
+        stage(branchName) {
+            def opts = getDockerOpt(config)
+            docker.image(image.url).inside(opts) {
+                runSteps(config)
+            }
+        }
+    }
+
+}
+
+Map getTasks(axes, image, config, include=null, exclude=null) {
+
+    Map tasks = [failFast: true]
+    for(int i = 0; i < axes.size(); i++) {
+        Map axis = axes[i]
+        axis.put("name", image.name)
+        axis.put("job", config.job)
+
+        if (exclude && matchMapEntry(exclude, axis)) {
+            echo "[INFO] Applying exclude filter on  " + axis.toMapString()
+            continue
+        } else if (include && ! matchMapEntry(include, axis)) {
+            echo "[INFO] Applying include filter on  " + axis.toMapString()
+            continue
+        }
+
+        def branchName = axis.values().join(', ')
+        //def branchName = "mypod-${UUID.randomUUID().toString()}"
+
+        // convert the Axis into valid values for withEnv step
+        if (config.get("env")) {
+            axis += config.env
+        }
+        List axisEnv = axis.collect { k, v ->
+            "${k}=${v}"
+        }
+
+        echo "[INFO] task name " + branchName
+        def arch = axis.arch
+        tasks[branchName] = { ->
+            withEnv(axisEnv) {
+                if(config.get("kubernetes")) {
+                    runK8(image, branchName, config, axis)
+                } else {
+                    runDocker(image, branchName, config, axis)
+                }
+            }
+        }
+    }
+    return tasks
+}
+Map getMatrixTasks(image, config) {
+    List axes = []
+    List include = null, exclude = null
+
+    if (config.get("matrix")) {
+        axes = getMatrixAxes(config.matrix.axes).findAll()
+        if (config.get("matrix").get("exclude")) {
+            exclude = config.matrix.exclude
+        }
+        if (config.get("matrix").get("include")) {
+            include = config.matrix.include
+        }
+    } else {
+        axes.add(image)
+    }
+
+    return getTasks(axes, image, config, include, exclude)
+}
+
+def buildImage(img, filename) {
+    unstash "${env.JOB_NAME}"
+    onUnstash()
+
+    if(filename == "") {
+        println("[ERROR] No docker filename specified, skipping build docker")
+        sh 'false'
+    }
+    juser = env.USER
+    juid  = sh(script: "id -u", returnStdout: true).trim()
+    jgid  = sh(script: "id -g", returnStdout: true).trim()
+
+    customImage = docker.build("${img}",
+        "--build-arg _UID=${juid} " +
+        "--build-arg _GID=${jgid} " +
+        "--build-arg _LOGIN=${juser} " +
+        "-f ${filename} . "
+        )
+    customImage.push()
+}
+
+
+def main() {
+    node("master") {
+
+        stage("Prepare checkout") {
+            forceCleanupWS()
+            checkout scm
+            
+            // create git tarball on server, agents will copy it and unpack
+            sh "tar cf scm-repo.tar .git"
+            stash includes: "scm-repo.tar", name: "${env.JOB_NAME}"
+        }
+
+        files = findFiles(glob: ".ci/${env.conf_file}")
+        files.each { file ->
+            def branches = [:]
+            def config = readYaml(file: file.path)
+            println ("New Job: " + config.job + " file: " + file.path)
+
+            def arch_distro_map = gen_image_map(config)
+            arch_distro_map.each { arch, images ->
+                images.each { image ->
+                    def img      = image.url
+                    def filename = image.filename
+                    def distro   = image.name
+
+                    def customImage
+                    node ("${arch} && docker") {
+                        stage("Prepare docker image for ${config.job}/$arch/$distro") {
+                            echo "Going to fetch docker image: ${img} from ${config.registry_host}"
+                            def need_build = 0
+                            docker.withRegistry("https://${config.registry_host}", config.registry_auth) {
+                                try {
+                                    echo("[INFO] Pulling image - ${img}")
+                                    customImage = docker.image(img).pull()
+                                } catch (exception) {
+                                    echo("[INFO] Image NOT found - ${img} - building ${filename} ...")
+                                    need_build++
+                                }
+
+                                if ("${env.build_dockers}" == "true") {
+                                    echo("[INFO] Forcing building file: ${filename} ... ")
+                                    need_build++
+                                }
+                                if (need_build) {
+                                    echo("[INFO] Building - ${img} - ${filename}")
+                                    buildImage(img, filename)
+                                }
+                            }
+                        }
+                    }
+                    branches += getMatrixTasks(image, config)
+                }
+            }
+        
+            try {
+                timestamps {
+                    parallel branches
+                }
+            } finally {
+                node("master") {
+                    stage("Finalize ${config.job}") {
+                        def cmd = config.steps?.finalize?.run[0]
+                        sh "${cmd}"
+                    }
+                }
+            }
+        }
+
+    }
+
+}
