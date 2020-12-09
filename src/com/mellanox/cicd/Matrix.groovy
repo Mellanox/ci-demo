ec6e134ee721641417ee762073f22e5ffc34acf0
Mike Dubman <miked@mellanox.com>
2020-12-09 17:16:54 +0200

    attempt to fix rare groovy issue: Caused: java.io.NotSerializableException: java.util.ArrayList

--- src/com/mellanox/cicd/Matrix.groovy
+++ src/com/mellanox/cicd/Matrix.groovy
@@ -269,6 +269,7 @@ def getDefaultShell(config=null, step=null, shell='#!/bin/bash -l') {
     return ret
 }
 
+@NonCPS
 def run_step(image, config, title, oneStep) {
 
     if (oneStep.get("enable") != null && !oneStep.enable) {
