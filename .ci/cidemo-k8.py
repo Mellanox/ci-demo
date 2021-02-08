890716515ed18750d4b64872cad7b52ef3c8b60e
Vasily Ryabov <vryabov@nvidia.com>
2021-02-08 15:46:37 +0300

    Use os.environ in cidemo-k8.py, fix the most of Python flake8 warnings.

--- .ci/cidemo-k8.py
+++ .ci/cidemo-k8.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-
 import os
-import sys
 import argparse
 import yaml
 from string import Template
@@ -9,7 +7,7 @@ from string import Template
 template = """apiVersion: v1
 kind: Pod
 metadata:
-  name: "{podname}"
+  name: "{podname}-debug-reproducer"
 spec:
   containers:
   - command:
@@ -48,107 +46,101 @@ mounts_tmpl = """
     readOnly: false
 """
 
-def usage() :
-	parser = argparse.ArgumentParser(description='Generate pod.yaml from ci-demo project file')
-	parser.add_argument('-file', metavar='filename', type=str, help='Path to job_matrix.yaml file', required=True)
-	parser.add_argument('-out',  metavar='filename', type=str, help='Output file for pod.yaml file, default: stdout', required=False)
-	parser.add_argument('-image_name', metavar='string', type=str, help="Select container name: default: find 1st from 'runs_on_dockers' list", required=False)
-	parser.add_argument('-arch', metavar='string', type=str, help='Select container arch, default: x86_64', required=False, default="x86_64")
-	parser.add_argument('-tag', metavar='string', type=str, help='Select container tag name, default: latest', required=False, default="latest")
 
-	args = parser.parse_args()
+def usage():
+    parser = argparse.ArgumentParser(description='Generate pod.yaml from ci-demo project file')
+    parser.add_argument('-file', metavar='filename', type=str, help='Path to job_matrix.yaml file', required=True)
+    parser.add_argument('-out',  metavar='filename', type=str, help='Output file for pod.yaml file, default: stdout', required=False)
+    parser.add_argument('-image_name', metavar='string', type=str, help="Select container name: default: find 1st from 'runs_on_dockers' list", required=False)
+    parser.add_argument('-arch', metavar='string', type=str, help='Select container arch, default: x86_64', required=False, default="x86_64")
+    parser.add_argument('-tag', metavar='string', type=str, help='Select container tag name, default: latest', required=False, default="latest")
 
-	if not os.path.isfile(args.file):
-		print('The file %s does not exist' % args.file)
-		sys.exit()
-	return args
+    args = parser.parse_args()
 
-def readJobProject(inFileName):
-	with open(inFileName) as inFile:
-		data = yaml.load(inFile)
-	return data
+    if not os.path.isfile(args.file):
+        print('The file {} does not exist'.format(args.file))
+        exit(1)
+    return args
 
-def resolveTemplate(str, config, image):
-	vars = {**config,**image}
-	t = Template(str)
 
-	if config.get('env'):
-		t.template = t.safe_substitute({**config['env']})
-		vars = {**config,**image,**config['env']}
+def read_job_project(in_file_name):
+    with open(in_file_name) as in_file:
+        data = yaml.safe_load(in_file)
+    return data
 
-	res = t.safe_substitute(vars)
-	return res
 
-def getYamlVal(yaml, key, default):
-	val = yaml.get(key)
-	if val:
-		return val
-	return default
+def resolve_template(str, config, image):
+    vars = {**config, **image}
+    t = Template(str)
 
-def generatePodYaml(args):
-	job_yaml = readJobProject(args.file)
-	image = None
+    if config.get('env'):
+        t.template = t.safe_substitute({**config['env']})
+        vars = {**config, **image, **config['env'], **os.environ}
 
-	for item in job_yaml['runs_on_dockers']:
-		if  args.image_name is None or args.image_name == item['name']:
-			image = item
-			break
+    res = t.safe_substitute(vars)
+    return res
 
-	if image is None:
-		print("Error: Image with name '" + args.image_name + "' is not found")
-		sys.exit()
-	
-	image['tag']  = getYamlVal(image, 'tag', args.tag)
-	image['arch'] = getYamlVal(image, 'arch', args.arch)
 
-	uri = getYamlVal(image, 'uri', image['arch'] + "/" + image['name'])
-	url = getYamlVal(image, 'url', job_yaml['registry_host'] + job_yaml['registry_path'] + "/" + uri + ":" + image['tag'])
+def generate_pod_yaml(args):
+    job_yaml = read_job_project(args.file)
+    image = None
 
-	image['uri'] = resolveTemplate(uri, job_yaml, image)
-	image['url'] = resolveTemplate(url, job_yaml, image)
-	
-	limits   = getYamlVal(job_yaml['kubernetes'], 'limits', '')
-	requests = getYamlVal(job_yaml['kubernetes'], 'requests', '')
-	nodeSelector = getYamlVal(job_yaml['kubernetes'], 'nodeSelector', '')
-	uid = getYamlVal(image, 'uid', '0')
-	gid = getYamlVal(image, 'gid', '0')
-	volumes = ''
-	volumeMounts = ''
-	vid = 0
-	for volume in job_yaml['volumes']:
-		volumes += volumes_tmpl.format(	
-										hostPath=volume['hostPath'],
-										volumeid=vid,
-										)
-		volumeMounts += volumeMounts.format( 	
-										mountPath=volume['mountPath'],
-										volumeid=vid
-										)
-		vid += 1
+    for item in job_yaml['runs_on_dockers']:
+        if args.image_name is None or args.image_name == item['name']:
+            image = item
+            break
 
-	pod_yaml = template.format(	podname=job_yaml['job'], 
-								imageurl=image['url'], 
-								imagename=image['name'], 
-								uid=uid, 
-								gid=gid,
-								limits=limits,
-								requests=requests,
-								node_selector=nodeSelector,
-								volumes=volumes,
-								volumeMounts=volumeMounts
-								)
+    if image is None:
+        print("Error: Image with name '{}' is not found".format(args.image_name))
+        exit(1)
 
-	pod_yaml = "".join([s for s in pod_yaml.strip().splitlines(True) if s.strip()])
-	if args.out is None:
-		fout = sys.stdout
-	else:
-		fout = open(args.out, "w")
+    image['tag'] = image.get('tag', args.tag)
+    image['arch'] = image.get('arch', args.arch)
 
-	print(pod_yaml, file=fout)
+    uri = image.get('uri', image['arch'] + "/" + image['name'])
+    url = image.get('url', job_yaml['registry_host'] + job_yaml['registry_path'] + "/" + uri + ":" + image['tag'])
 
-def main():
-	args = usage()
-	generatePodYaml(args)
+    image['uri'] = resolve_template(uri, job_yaml, image)
+    image['url'] = resolve_template(url, job_yaml, image)
+
+    limits = job_yaml['kubernetes'].get('limits', '')
+    requests = job_yaml['kubernetes'].get('requests', '')
+    nodeSelector = job_yaml['kubernetes'].get('nodeSelector', '')
+    uid = image.get('uid', '0')
+    gid = image.get('gid', '0')
+    volumes = ''
+    volumeMounts = ''
+    for vid, volume in enumerate(job_yaml['volumes']):
+        volumes += volumes_tmpl.format(
+            hostPath=volume['hostPath'],
+            volumeid=vid,
+        )
+        volumeMounts += volumeMounts.format(
+            mountPath=volume['mountPath'],
+            volumeid=vid
+        )
+
+    pod_yaml = template.format(
+        podname=job_yaml['job'],
+        imageurl=image['url'],
+        imagename=image['name'],
+        uid=uid,
+        gid=gid,
+        limits=limits,
+        requests=requests,
+        node_selector=nodeSelector,
+        volumes=volumes,
+        volumeMounts=volumeMounts
+    )
+
+    pod_yaml = "".join([s for s in pod_yaml.strip().splitlines(True) if s.strip()])
+    if args.out is None:
+        print(pod_yaml)
+    else:
+        with open(args.out, "w") as fout:
+            print(pod_yaml, file=fout)
+
 
 if __name__ == '__main__':
-    main()
+    args = usage()
+    generate_pod_yaml(args)
