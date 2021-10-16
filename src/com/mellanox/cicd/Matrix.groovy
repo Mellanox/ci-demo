a8c79cc523d5b3513fe43cf80c2acb7f5ba78d81
Mike Dubman <miked@nvidia.com>
2021-10-16 18:49:10 +0300

    Add regex support in containerSelector, agentSelector and matrix filters

--- src/com/mellanox/cicd/Matrix.groovy
+++ src/com/mellanox/cicd/Matrix.groovy
@@ -258,7 +258,7 @@ def matchMapEntry(filters, entry) {
     for (int i=0; i<filters.size(); i++) {
         match = true
         filters[i].each { k, v ->
-            if (entry[k] == null || v != entry[k]) {
+            if (entry[k] == null || !entry[k].matches(v)) {
                 match = false
             }
         }
