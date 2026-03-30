805cf3bf53ef92afd1d16dcc322c2588d9554375
Mike Dubman <mdubman@nvidia.com>
2026-03-30 14:45:31 +0300

    Fix Jenkins 2.54x CLI root URL; pin LTS image tag
    - Set JenkinsLocationConfiguration URL in disable-security init (default
      http://localhost:8080); pass JENKINS_LOCAL_ROOT_URL from local_gha_ci.sh.
    - Pin FROM jenkins/jenkins:2.541.3-lts-jdk17 so :lts-jdk17 drift does not
      break CI; CLI jar still matches bundled jenkins.war version.

--- scripts/local_gha_ci.sh
+++ scripts/local_gha_ci.sh
@@ -250,6 +250,7 @@ docker run -d --name "${JENKINS_NAME}" \
   -e JENKINS_K8S_JENKINS_URL="http://${JENKINS_K8S_DNS_NAME}:8080" \
   -e JENKINS_K8S_JENKINS_TUNNEL="${JENKINS_K8S_DNS_NAME}:50000" \
   -e JENKINS_K8S_NAMESPACE="${JENKINS_K8S_NAMESPACE:-default}" \
+  -e JENKINS_LOCAL_ROOT_URL="${JENKINS_LOCAL_ROOT_URL:-http://localhost:8080}" \
   -e JAVA_OPTS="-Djenkins.install.runSetupWizard=false -Dhudson.security.csrf.GlobalCrumbIssuerConfiguration.DISABLE_CSRF_PROTECTION=true -Dpermissive-script-security.enabled=no_security -Dhudson.plugins.git.GitSCM.ALLOW_LOCAL_CHECKOUT=true" \
   "${JENKINS_IMAGE}" \
   /usr/local/bin/jenkins.sh >/dev/null
